aa618f5c53da220368f94497c95c4ee408bff106
SchmidtKrisztian <schmidtkrisz98k@gmail.com>
2020-11-03 15:52:30 +0100

    Editing IBrandRepository

--- OENIK_PROG3_2020_2_A1LAX5/MyLaptopShop.Repository/Interfaces/IBrandRepository.cs
+++ OENIK_PROG3_2020_2_A1LAX5/MyLaptopShop.Repository/Interfaces/IBrandRepository.cs
@@ -18,28 +18,28 @@ namespace MyLaptopShop.Repository.Interfaces
         /// Method signature, you can change the Name of a brand with it.
         /// </summary>
         /// <param name="id">Int, the brand with this ID, will have his name changed.</param>
-        /// <param name="name">String, the new name of a Brand.</param>
-        void ChangeName(int id, string name);
+        /// <param name="newname">String, the new name of a Brand.</param>
+        void ChangeName(int id, string newname);
 
         /// <summary>
         /// Method signature, you can change the brands year of foundation.
         /// </summary>
         /// <param name="id">Int, the brand with this ID, will have his foundation year changed.</param>
-        /// <param name="year">Int, this will me the new foundation year.</param>
-        void ChangeFoundationYear(int id, int year);
+        /// <param name="newyear">Int, this will me the new foundation year.</param>
+        void ChangeFoundationYear(int id, int newyear);
 
         /// <summary>
         /// Method signature, you can change the brands headquarters place.
         /// </summary>
         /// <param name="id">Int, the brand headquarters with this id, will be changed.</param>
-        /// <param name="headquarters">String, this wis the new Headquarters.</param>
-        void ChangeHeadguarters(int id, string headquarters);
+        /// <param name="newheadquarters">String, this wis the new Headquarters.</param>
+        void ChangeHeadguarters(int id, string newheadquarters);
 
         /// <summary>
         /// Method signature, you can change the brands CEOs name.
         /// </summary>
         /// <param name="id">Int, the brand with this ID, will have his CEOs name changed.</param>
-        /// <param name="ceoname">String, the new CEO Name.</param>
-        void ChangeCEOName(int id, string ceoname);
+        /// <param name="newceoname">String, the new CEO Name.</param>
+        void ChangeCEOName(int id, string newceoname);
     }
 }
