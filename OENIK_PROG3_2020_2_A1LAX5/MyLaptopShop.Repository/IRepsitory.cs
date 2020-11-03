77799b79be7b0e379b359f79843a77576a15fdc4
SchmidtKrisztian <schmidtkrisz98k@gmail.com>
2020-11-03 15:10:04 +0100

    Editing IRepository interface

--- OENIK_PROG3_2020_2_A1LAX5/MyLaptopShop.Repository/IRepsitory.cs
+++ OENIK_PROG3_2020_2_A1LAX5/MyLaptopShop.Repository/IRepsitory.cs
@@ -8,11 +8,25 @@ namespace MyLaptopShop.Repository
     using System.Collections.Generic;
     using System.Linq;
     using System.Text;
+
+    /// <summary>
+    /// This is the base interface of the Resopitory layer.
+    /// </summary>
+    /// <typeparam name="T">Generic type, T is a class.</typeparam>
     public interface IRepsitory<T>
          where T : class
      {
+        /// <summary>
+        /// Method signature.
+        /// </summary>
+        /// <param name="id">Wants an int id.</param>
+        /// <returns>Retruns an object that has the parameter id.</returns>
         T GetOne(int id);
 
+        /// <summary>
+        /// Method signature.
+        /// </summary>
+        /// <returns>All the objects.</returns>
         IQueryable<T> GetAll();
      }
 }
