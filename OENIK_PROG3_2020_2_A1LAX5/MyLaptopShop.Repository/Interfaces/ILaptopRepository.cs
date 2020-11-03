e6f4fca0182feae9b459407f9a5e6c6cfcbc0070
SchmidtKrisztian <schmidtkrisz98k@gmail.com>
2020-11-03 16:04:46 +0100

    Editing ILaptopRepository interface

--- OENIK_PROG3_2020_2_A1LAX5/MyLaptopShop.Repository/Interfaces/ILaptopRepository.cs
+++ OENIK_PROG3_2020_2_A1LAX5/MyLaptopShop.Repository/Interfaces/ILaptopRepository.cs
@@ -1,10 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿// <copyright file="ILaptopRepository.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 
 namespace MyLaptopShop.Repository.Interfaces
 {
-    interface ILaptopRepository
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using MyLaptopShop.Data.Models;
+
+    /// <summary>
+    /// This is the Interface of the LaptopRepository.
+    /// </summary>
+    public interface ILaptopRepository : IRepository<Laptop>
     {
+        /// <summary>
+        /// Method signature, you can change the name of a laptop with it.
+        /// </summary>
+        /// <param name="id">Int, the laptop with this ID, will have his name changed.</param>
+        /// <param name="newname">String, the new name of a laptop.</param>
+        void ChangeName(int id, string newname);
+
+        /// <summary>
+        /// Method signature, you can change the relese year of a laptop with it.
+        /// </summary>
+        /// <param name="id">Int, the laptop with this ID, will have his release year changed.</param>
+        /// <param name="newreleaseyear">int, the new name of a Brand.</param>
+        void ChangeReleaseYear(int id, string newreleaseyear);
+
+        /// <summary>
+        /// Method signature, you can change the base price of a laptop with it.
+        /// </summary>
+        /// <param name="id">Int, the laptop with this ID, will have his base price changed.</param>
+        /// <param name="newbaseprice">Int, the new base price of a laptop.</param>
+        void ChangeBasePrice(int id, int newbaseprice);
     }
 }
