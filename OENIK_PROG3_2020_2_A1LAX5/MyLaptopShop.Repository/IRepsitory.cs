80c83f51a79681b46fcebe8ea98343d277da88b4
SchmidtKrisztian <schmidtkrisz98k@gmail.com>
2020-11-03 14:55:05 +0100

    Creating IRepository interface in the repository library

---
+++ OENIK_PROG3_2020_2_A1LAX5/MyLaptopShop.Repository/IRepsitory.cs
@@ -0,0 +1,18 @@
+﻿// <copyright file="IRepsitory.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace MyLaptopShop.Repository
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    public interface IRepsitory<T>
+         where T : class
+     {
+        T GetOne(int id);
+
+        IQueryable<T> GetAll();
+     }
+}
