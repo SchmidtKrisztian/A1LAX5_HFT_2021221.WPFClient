4503a6fe13f912ea22792f0b9600c2099d4ca15e
SchmidtKrisztian <schmidtkrisz98k@gmail.com>
2020-11-10 19:50:30 +0100

    Adding Menu class

---
+++ OENIK_PROG3_2020_2_A1LAX5/MyLaptopShop.Program/Menu.cs
@@ -0,0 +1,26 @@
+﻿// <copyright file="Menu.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace MyLaptopShop.Program
+{
+    using MyLaptopShop.Logic.Classes;
+    using MyLaptopShop.Logic.Interfaces;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// This is the Menu class.
+    /// </summary>
+    public class Menu
+    {
+        private LaptopLogic laptopLogic;
+
+        public Menu()
+        {
+            this.laptopLogic = new LaptopLogic();
+        }
+
+    }
+}
