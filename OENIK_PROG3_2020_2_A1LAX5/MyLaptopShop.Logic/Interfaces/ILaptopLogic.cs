4b5fe7ca129105e33530b9129aabff93ed7ef161
SchmidtKrisztian <schmidtkrisz98k@gmail.com>
2020-11-10 15:22:43 +0100

    Adding ILaptopLogic interface

---
+++ OENIK_PROG3_2020_2_A1LAX5/MyLaptopShop.Logic/Interfaces/ILaptopLogic.cs
@@ -0,0 +1,19 @@
+﻿using MyLaptopShop.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyLaptopShop.Logic.Interfaces
+{
+    interface ILaptopLogic<T>
+        where T : class
+    {
+        Laptop GetLaptopById(int id);
+
+        void Update(T istance, string property, string newname);
+
+        IList<T> GetAllLaptops();
+
+        void DeleteLaptop(int id);
+    }
+}
