59ab3a66d6641b8e6acd43b10ad5c9dbc74e4a72
SchmidtKrisztian <schmidtkrisz98k@gmail.com>
2020-11-10 21:45:50 +0100

    Adding program.cs

---
+++ OENIK_PROG3_2020_2_A1LAX5/MyLaptopShop.Program/Program.cs
@@ -0,0 +1,22 @@
+﻿// <copyright file="Program.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace MyLaptopShop.Program
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    /// <summary>
+    /// This is the main class.
+    /// </summary>
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Factory factory = new Factory();
+            Menu menu = new Menu();
+            MainMenu();
+        }
+    }
+}
