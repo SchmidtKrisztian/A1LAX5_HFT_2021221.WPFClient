3369524bf2ffa4a9537e77d711103676d7543bd0
SchmidtKrisztian <schmidtkrisz98k@gmail.com>
2020-11-10 19:50:12 +0100

    Adding Factory class

---
+++ OENIK_PROG3_2020_2_A1LAX5/MyLaptopShop.Program/Factory.cs
@@ -0,0 +1,38 @@
+﻿// <copyright file="Factory.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace MyLaptopShop.Program
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using MyLaptopShop.Data.Models;
+    using MyLaptopShop.Logic.Classes;
+    using MyLaptopShop.Repository.Classes;
+
+    /// <summary>
+    /// Factory class to make an instances for alll the classses.
+    /// </summary>
+    public class Factory
+    {
+        private static LaptopShopContext ctx = new LaptopShopContext();
+        private static BrandRepository brandrepo = new BrandRepository(ctx);
+        private static LaptopRepository laptoprepo = new LaptopRepository(ctx);
+        private static SpecificationRepository specrepo = new SpecificationRepository(ctx);
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Factory"/> class.
+        /// </summary>
+        public Factory()
+        {
+            this.Laptoplogic = new LaptopLogic(brandrepo, laptoprepo, specrepo);
+        }
+
+        /// <summary>
+        /// Gets or sets the Laptoplogic instance.
+        /// </summary>
+        public LaptopLogic Laptoplogic { get; set; }
+
+    }
+}
