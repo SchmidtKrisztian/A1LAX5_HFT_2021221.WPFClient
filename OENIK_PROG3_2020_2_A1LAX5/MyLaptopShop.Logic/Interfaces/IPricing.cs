c3573c0af664119bbbbcc14d4e0c5c2e4cbfddb0
SchmidtKrisztian <schmidtkrisz98k@gmail.com>
2020-11-10 16:55:53 +0100

    Editing IPricing interface

---
+++ OENIK_PROG3_2020_2_A1LAX5/MyLaptopShop.Logic/Interfaces/IPricing.cs
@@ -0,0 +1,40 @@
+﻿// <copyright file="IPricing.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace MyLaptopShop.Logic.Interfaces
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// Interface of the Pricing class.
+    /// </summary>
+    public interface IPricing
+    {
+        /// <summary>
+        /// Constructor of the brand name.
+        /// </summary>
+        /// <returns>Get,Set.</returns>
+        string Brand();
+
+        /// <summary>
+        /// Constructor of the laptop name.
+        /// </summary>
+        /// <returns>Get,Set.</returns>
+        string Laptopname();
+
+        /// <summary>
+        /// Constructor of the specification name.
+        /// </summary>
+        /// <returns>Get,Set.</returns>
+        string Specname();
+
+        /// <summary>
+        /// Constructor of the fullprice.
+        /// </summary>
+        /// <returns>Gets,Sets.</returns>
+        int Fullprice();
+    }
+}
