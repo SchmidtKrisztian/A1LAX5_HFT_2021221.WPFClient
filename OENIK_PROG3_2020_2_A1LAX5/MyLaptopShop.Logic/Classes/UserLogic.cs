7d8678b1559a489cfc47c8551b3cadc6ebc723db
SchmidtKrisztian <schmidtkrisz98k@gmail.com>
2020-11-13 17:17:59 +0100

    Editing UserLogic

---
+++ OENIK_PROG3_2020_2_A1LAX5/MyLaptopShop.Logic/Classes/UserLogic.cs
@@ -0,0 +1,112 @@
+﻿// <copyright file="UserLogic.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace MyLaptopShop.Logic.Classes
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using MyLaptopShop.Data.Models;
+    using MyLaptopShop.Logic.Interfaces;
+    using MyLaptopShop.Repository.Interfaces;
+
+    /// <summary>
+    /// Class that does the listing methods.
+    /// </summary>
+    public class UserLogic : IUserLogic
+    {
+        private IBrandRepository brandrepo;
+        private ILaptopRepository laptoprepo;
+        private ISpecificationRepository specrepo;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UserLogic"/> class.
+        /// </summary>
+        /// <param name="brandrepo">Brand repository instance.</param>
+        /// <param name="laptoprepo">Laptop repository instance.</param>
+        /// <param name="specrepo">Specification repository instance.</param>
+        public UserLogic(IBrandRepository brandrepo, ILaptopRepository laptoprepo, ISpecificationRepository specrepo)
+        {
+            this.brandrepo = brandrepo;
+            this.laptoprepo = laptoprepo;
+            this.specrepo = specrepo;
+        }
+
+        /// <summary>
+        /// Gives back the istance witch belongs to the parameter ID.
+        /// </summary>
+        /// <param name="id">ID of the parameter we want to get.</param>
+        /// <returns>The instance with the parameter ID.</returns>
+        public Brand BrandGetOne(int id)
+        {
+            return this.brandrepo.GetOne(id);
+        }
+
+        /// <summary>
+        /// Gives back the istance witch belongs to the parameter ID.
+        /// </summary>
+        /// <param name="id">ID of the parameter we want to get.</param>
+        /// <returns>The instance with the parameter ID.</returns>
+        public Laptop LaptopGetOne(int id)
+        {
+            return this.laptoprepo.GetOne(id);
+        }
+
+        /// <summary>
+        /// Gives back the istance witch belongs to the parameter ID.
+        /// </summary>
+        /// <param name="id">ID of the parameter we want to get.</param>
+        /// <returns>The instance with the parameter ID.</returns>
+        public Specification SpecGetOne(int id)
+        {
+            return this.specrepo.GetOne(id);
+        }
+
+        /// <summary>
+        /// Gives back all the brands.
+        /// </summary>
+        /// <returns>List of all brands.</returns>
+        public IList<Brand> GetAllBrand()
+        {
+            return this.brandrepo.GetAll().ToList();
+        }
+
+        /// <summary>
+        /// Gives back all the laptops.
+        /// </summary>
+        /// <returns>List of all laptops.</returns>
+        public IList<Laptop> GetAllLaptop()
+        {
+            return this.laptoprepo.GetAll().ToList();
+        }
+
+        /// <summary>
+        /// Gives back all the specifications.
+        /// </summary>
+        /// <returns>List of all specifications.</returns>
+        public IList<Specification> GetAllSpec()
+        {
+            return this.specrepo.GetAll().ToList();
+        }
+
+        /// <summary>
+        /// List the number of laptops group by brands.
+        /// </summary>
+        /// <returns>List of strings.</returns>
+        public IList<string> LaptopCount()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Lists laptops with average specification prices.
+        /// </summary>
+        /// <returns>List of fromed strings of the results.</returns>
+        public IList<string> AvgSpecPrice()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
