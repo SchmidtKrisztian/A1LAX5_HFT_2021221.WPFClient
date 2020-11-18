68a70e00974fdf78a523b94c962d68e3654a9ccc
SchmidtKrisztian <schmidtkrisz98k@gmail.com>
2020-11-18 08:51:30 +0100

    Creating UserLogicTest.cs

---
+++ OENIK_PROG3_2020_2_A1LAX5/MyLaptopShop.Logic.Tests/UserLogicTest.cs
@@ -0,0 +1,28 @@
+﻿using Moq;
+using MyLaptopShop.Logic.Classes;
+using MyLaptopShop.Repository.Classes;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyLaptopShop.Logic.Tests
+{
+    /// <summary>
+    /// A class to test the UserLogic.
+    /// </summary>
+    class UserLogicTest
+    {
+        private static UserLogic userlogic;
+
+        private static Mock<BrandRepository> brandRepoMock;
+        private static Mock<LaptopRepository> laptopRepoMock;
+        private static Mock<SpecificationRepository> specRepoMock;
+
+        [SetUp]
+        public static void Setup()
+        {
+
+        }
+    }
+}
