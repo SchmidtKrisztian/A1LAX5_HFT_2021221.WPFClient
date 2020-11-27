679303a882e301a79b22faf6291d327dd165ba99
SchmidtKrisztian <schmidtkrisz98k@gmail.com>
2020-11-27 15:13:27 +0100

    Supressing warning

--- OENIK_PROG3_2020_2_A1LAX5/MyLaptopShop.Logic/GlobalSuppressions.cs
+++ OENIK_PROG3_2020_2_A1LAX5/MyLaptopShop.Logic/GlobalSuppressions.cs
@@ -1,8 +1,7 @@
-﻿// This file is used by Code Analysis to maintain SuppressMessage
-// attributes that are applied to this project.
-// Project-level suppressions either have no target or are given
-// a specific target and scoped to a namespace, type, member, etc.
+﻿// <copyright file="GlobalSuppressions.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 
 using System.Diagnostics.CodeAnalysis;
 
-[assembly: SuppressMessage("Globalization", "CA1304:Specify CultureInfo", Justification = "<Pending>", Scope = "member", Target = "~M:MyLaptopShop.Logic.Classes.UserLogic.GamerBrand~System.Collections.Generic.IList{System.String}")]
+[assembly: SuppressMessage("Globalization", "CA1304:Specify CultureInfo", Justification = "This is a dumb warning.", Scope = "member", Target = "~M:MyLaptopShop.Logic.Classes.UserLogic.GamerBrand~System.Collections.Generic.IList{System.String}")]
