759a7d21f164a549f919a431574060212ae9dfb6
SchmidtKrisztian <schmidtkrisz98k@gmail.com>
2020-12-05 20:29:33 +0100

    Suppressing the new warnings

--- OENIK_PROG3_2020_2_A1LAX5/MyLaptopShop.Data/GlobalSuppressions.cs
+++ OENIK_PROG3_2020_2_A1LAX5/MyLaptopShop.Data/GlobalSuppressions.cs
@@ -6,3 +6,4 @@ using System.Diagnostics.CodeAnalysis;
 
 [assembly: SuppressMessage("Usage", "CA2227:Collection properties should be read only", Justification = "We dont want it to be read only.", Scope = "member", Target = "~P:MyLaptopShop.Data.Models.Laptop.Specifications")]
 [assembly: SuppressMessage("Usage", "CA2227:Collection properties should be read only", Justification = "We dont want it to be read only.", Scope = "member", Target = "~P:MyLaptopShop.Data.Models.Brand.Laptops")]
+[assembly: SuppressMessage("Maintainability", "CA9998:FxCopAnalyzers package has been deprecated", Justification = "FCK 2020", Scope = "module")]
--- OENIK_PROG3_2020_2_A1LAX5/MyLaptopShop.Repository/GlobalSuppressions.cs
+++ OENIK_PROG3_2020_2_A1LAX5/MyLaptopShop.Repository/GlobalSuppressions.cs
@@ -4,5 +4,4 @@
 
 using System.Diagnostics.CodeAnalysis;
 
-[assembly: SuppressMessage("Globalization", "CA1304:Specify CultureInfo", Justification = "This is a dumb warning.", Scope = "member", Target = "~M:MyLaptopShop.Logic.Classes.UserLogic.GamerBrand~System.Collections.Generic.IList{System.String}")]
 [assembly: SuppressMessage("Maintainability", "CA9998:FxCopAnalyzers package has been deprecated", Justification = "FCK 2020", Scope = "module")]
