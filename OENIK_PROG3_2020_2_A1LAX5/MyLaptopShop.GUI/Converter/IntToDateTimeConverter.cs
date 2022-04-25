7dba6224a1f41b2b2ade47aa304b469cad73a48c
SchmidtKrisztian <schmidtkrisz98k@gmail.com>
2022-04-25 19:38:16 +0200

    Add Converter/IntToDateTimeConverter.cs

---
+++ OENIK_PROG3_2020_2_A1LAX5/MyLaptopShop.GUI/Converter/IntToDateTimeConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace MyLaptopShop.GUI.Converter
+{
+    public class IntToDateTimeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is int i && i > 0)
+            {
+                return new DateTime(i, 1, 1); 
+            }
+            return new DateTime(1970, 1, 1);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is DateTime dt && dt.Year > 0)
+            {
+                return dt.Year;
+            }
+            return 1970;
+        }
+    }
+}
